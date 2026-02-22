a7205a3e17fee589ff325ff294cbb7c0133dd38f
DELL <DELL@DELL-PC>
2026-02-22 13:11:18 +0530

    Add project files.

---
+++ TextCompletion/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+using OpenAI;
+using OpenAI.Chat;
+using System.ClientModel;
+using System.Text.Json.Serialization;
+
+IConfigurationRoot config=new  ConfigurationBuilder().AddUserSecrets<Program>().Build();
+var credential = new ApiKeyCredential(config["GitHubModels:Token"] ?? throw new InvalidOperationException("Missing configuration: GitHubModels:Token."));
+var options = new OpenAIClientOptions()
+{
+    Endpoint = new Uri("https://models.github.ai/inference")
+};
+IChatClient client =
+    new OpenAIClient(credential, options).GetChatClient("openai/o4-mini").AsIChatClient(); 
+ChatResponse response = await client.GetResponseAsync("what is  AI explain max in 20 words");
+
+Console.WriteLine(response);
